0cd028b94c004c23122b805bb986c97fc4618f1d
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-02-12 10:14:05 +0200

    fixup! Drop language version 1.7
    Drop obsolete 1.7 tests

--- js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/FirJsIrDeserializationCodegenBoxTestGenerated.java
+++ js/js.tests/tests-gen/org/jetbrains/kotlin/js/test/ir/FirJsIrDeserializationCodegenBoxTestGenerated.java
@@ -7450,12 +7450,6 @@ public class FirJsIrDeserializationCodegenBoxTestGenerated extends AbstractFirJs
       }
 
       @Test
-      @TestMetadata("kt12908_2.kt")
-      public void testKt12908_2() {
-        runTest("compiler/testData/codegen/box/controlStructures/kt12908_2.kt");
-      }
-
-      @Test
       @TestMetadata("kt1441.kt")
       public void testKt1441() {
         runTest("compiler/testData/codegen/box/controlStructures/kt1441.kt");
@@ -18487,12 +18481,6 @@ public class FirJsIrDeserializationCodegenBoxTestGenerated extends AbstractFirJs
       @TestMetadata("kt39824.kt")
       public void testKt39824() {
         runTest("compiler/testData/codegen/box/inference/kt39824.kt");
-      }
-
-      @Test
-      @TestMetadata("kt42042.kt")
-      public void testKt42042() {
-        runTest("compiler/testData/codegen/box/inference/kt42042.kt");
       }
 
       @Test
@@ -43195,12 +43183,6 @@ public class FirJsIrDeserializationCodegenBoxTestGenerated extends AbstractFirJs
       @TestMetadata("exceptionOnNoMatch.kt")
       public void testExceptionOnNoMatch() {
         runTest("compiler/testData/codegen/box/when/exceptionOnNoMatch.kt");
-      }
-
-      @Test
-      @TestMetadata("exhaustiveBoolean.kt")
-      public void testExhaustiveBoolean() {
-        runTest("compiler/testData/codegen/box/when/exhaustiveBoolean.kt");
       }
 
       @Test
--- native/native.tests/klib-ir-inliner/tests-gen/org/jetbrains/kotlin/konan/test/serialization/FirNativeSerializationTestGenerated.java
+++ native/native.tests/klib-ir-inliner/tests-gen/org/jetbrains/kotlin/konan/test/serialization/FirNativeSerializationTestGenerated.java
@@ -8026,12 +8026,6 @@ public class FirNativeSerializationTestGenerated extends AbstractFirNativeSerial
       }
 
       @Test
-      @TestMetadata("kt12908_2.kt")
-      public void testKt12908_2() {
-        runTest("compiler/testData/codegen/box/controlStructures/kt12908_2.kt");
-      }
-
-      @Test
       @TestMetadata("kt1441.kt")
       public void testKt1441() {
         runTest("compiler/testData/codegen/box/controlStructures/kt1441.kt");
@@ -19063,12 +19057,6 @@ public class FirNativeSerializationTestGenerated extends AbstractFirNativeSerial
       @TestMetadata("kt39824.kt")
       public void testKt39824() {
         runTest("compiler/testData/codegen/box/inference/kt39824.kt");
-      }
-
-      @Test
-      @TestMetadata("kt42042.kt")
-      public void testKt42042() {
-        runTest("compiler/testData/codegen/box/inference/kt42042.kt");
       }
 
       @Test
@@ -43787,12 +43775,6 @@ public class FirNativeSerializationTestGenerated extends AbstractFirNativeSerial
       @TestMetadata("exceptionOnNoMatch.kt")
       public void testExceptionOnNoMatch() {
         runTest("compiler/testData/codegen/box/when/exceptionOnNoMatch.kt");
-      }
-
-      @Test
-      @TestMetadata("exhaustiveBoolean.kt")
-      public void testExhaustiveBoolean() {
-        runTest("compiler/testData/codegen/box/when/exhaustiveBoolean.kt");
       }
 
       @Test
