272b903d1f9b85d8e1e510f8a7dc41de1c4b4859
Adam Semenenko <adam.semenenko@jetbrains.com>
2025-02-26 14:10:26 +0100

    fixup! [Gradle] Tidy KGP integration test utilities
    Fully update MppHighlightingTestDataWithGradleIT to use NIO.

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/MppHighlightingTestDataWithGradleIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/MppHighlightingTestDataWithGradleIT.kt
@@ -13,8 +13,6 @@ import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsSource
-import java.io.File
-import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Stream
 import kotlin.io.path.*
@@ -33,7 +31,8 @@ internal class MppHighlightingTestDataWithGradleIT : KGPBaseTest() {
     ) {
         project("mpp-source-set-hierarchy-analysis", gradleVersion) {
             val expectedErrorsPerSourceSetName = sourceRoots.associate { sourceRoot ->
-                sourceRoot.kotlinSourceSetName to testDataDir.resolve(sourceRoot.directoryName).walk()
+                sourceRoot.kotlinSourceSetName to testDataDir.resolve(sourceRoot.directoryName)
+                    .walk()
                     .filter { it.extension == "kt" }
                     .map { CodeWithErrorInfo.parse(it.readText()) }
                     .flatMap { it.errorInfo }
@@ -184,53 +183,51 @@ internal class MppHighlightingTestDataWithGradleIT : KGPBaseTest() {
     }
 
     class GradleAndMppHighlightingProvider : GradleArgumentsProvider() {
-        private val testDataRoot = File("../../../idea/testData/multiModuleHighlighting/multiplatform")
+        private val testDataRoot = Path("../../../idea/testData/multiModuleHighlighting/multiplatform")
 
         private val bannedDependencies = setOf("fulljdk", "stdlib", "coroutines")
 
         private fun isTestSuiteValidForCommonCode(
-            testDataDir: File,
+            testDataDir: Path,
             sourceRoots: List<TestCaseSourceRoot>,
         ): Boolean = when {
             sourceRoots.any { bannedDependencies.intersect(it.dependencies.toSet()).isNotEmpty() } -> false
             // Java sources can't be used in intermediate source sets
-            testDataDir.walkTopDown().any { it.extension == "java" } -> false
+            testDataDir.walk().any { it.extension == "java" } -> false
             // Cannot test CHECK_HIGHLIGHTING in CLI
-            testDataDir.walkTopDown().filter { it.isFile }.any { "CHECK_HIGHLIGHTING" in it.readText() } -> false
+            testDataDir.walk().filter { it.isRegularFile() }.any { "CHECK_HIGHLIGHTING" in it.readText() } -> false
             else -> true
         }
 
-        private val testData = testDataRoot
-            .walkTopDown()
-            .maxDepth(1)
-            .filter { it.isDirectory && Files.newDirectoryStream(it.toPath()).use { stream -> stream.toList().isNotEmpty() } }
-            .map { testDataDir ->
-                Pair(
-                    testDataDir,
-                    Files.newDirectoryStream(testDataDir.toPath()).use { stream ->
-                        stream.map { TestCaseSourceRoot.parse(it.fileName.toString()) }
+        private val testData: List<Pair<Path, List<TestCaseSourceRoot>>> =
+            testDataRoot
+                .listDirectoryEntries()
+                .filter { it.isDirectory() && it.listDirectoryEntries().isNotEmpty() }
+                .map { testDataDir ->
+                    testDataDir to testDataDir.useDirectoryEntries { entries ->
+                        entries.map { TestCaseSourceRoot.parse(it.name) }.toList()
                     }
-                )
-            }
-            .filter {
-                isTestSuiteValidForCommonCode(it.first, it.second)
-            }
-            .toList()
+                }
+                .filter { (testDataDir, sourceRoots) ->
+                    isTestSuiteValidForCommonCode(testDataDir, sourceRoots)
+                }
 
         override fun provideArguments(
             context: ExtensionContext,
         ): Stream<out Arguments> {
-            val gradleVersions = super.provideArguments(context).map { it.get().first() as GradleVersion }.toList()
+            val gradleVersions = super.provideArguments(context)
+                .map { it.get().first() as GradleVersion }
+                .toList()
 
             return gradleVersions
+                .asSequence()
                 .flatMap { gradleVersion ->
                     CliCompiler.entries.flatMap { cliCompiler ->
-                        testData.map {
-                            Arguments.of(gradleVersion, cliCompiler, it.first, it.second)
+                        testData.map { (testDataDir, sourceRoots) ->
+                            Arguments.of(gradleVersion, cliCompiler, testDataDir, sourceRoots)
                         }
                     }
                 }
-                .asSequence()
                 .asStream()
         }
     }
