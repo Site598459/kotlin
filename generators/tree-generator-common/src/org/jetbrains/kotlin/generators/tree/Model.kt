ed7a25654004924d67125847678bf7086e17bc7e
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2025-02-27 18:27:34 +0100

    [Tree generators] Move addPureAbstractElement to Model

--- generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/Model.kt
+++ generators/tree-generator-common/src/org/jetbrains/kotlin/generators/tree/Model.kt
@@ -23,4 +23,14 @@ data class Model<Element : AbstractElement<Element, *, *>>(
             recurse(element)
         }
     }
+
+    fun addPureAbstractElement(pureAbstractElement: ClassRef<*>) {
+        for (el in elements) {
+            if (el.kind?.isInterface != true &&
+                el.allParents.none<ImplementationKindOwner> { it.kind == ImplementationKind.AbstractClass || it.kind == ImplementationKind.SealedClass }
+            ) {
+                el.otherParents.add(pureAbstractElement)
+            }
+        }
+    }
 }
