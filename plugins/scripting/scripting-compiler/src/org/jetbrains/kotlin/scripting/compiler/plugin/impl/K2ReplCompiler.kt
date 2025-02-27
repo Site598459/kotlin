df9aeb6b68eb8e3651ce27185f5b429e545d3ccf
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2025-02-27 15:00:16 +0100

    K2 REPL: Rearrange snippet imports processing
    making it more similar to the script one, but also moving
    the imports provider to the repl-specific extension
    
    #KT-74863 in progress

--- plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/impl/K2ReplCompiler.kt
+++ plugins/scripting/scripting-compiler/src/org/jetbrains/kotlin/scripting/compiler/plugin/impl/K2ReplCompiler.kt
@@ -24,7 +24,6 @@ import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.config.JvmTarget
 import org.jetbrains.kotlin.config.languageVersionSettings
 import org.jetbrains.kotlin.diagnostics.DiagnosticReporterFactory
-import org.jetbrains.kotlin.fir.FirImplementationDetail
 import org.jetbrains.kotlin.fir.FirModuleCapabilities
 import org.jetbrains.kotlin.fir.FirModuleData
 import org.jetbrains.kotlin.fir.FirModuleDataImpl
@@ -49,7 +48,6 @@ import org.jetbrains.kotlin.scripting.compiler.plugin.dependencies.collectScript
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.FirReplHistoryProviderImpl
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.firReplHistoryProvider
 import org.jetbrains.kotlin.scripting.compiler.plugin.services.isReplSnippetSource
-import org.jetbrains.kotlin.scripting.compiler.plugin.services.replCompilationConfigurationProviderService
 import org.jetbrains.kotlin.scripting.definitions.ScriptConfigurationsProvider
 import java.io.File
 import java.nio.file.Path
@@ -331,9 +329,6 @@ private fun compileImpl(
         needRegisterJavaElementFinder = true,
         init = {},
     )
-    @OptIn(FirImplementationDetail::class)
-    session.replCompilationConfigurationProviderService.scriptConfiguration = state.scriptCompilationConfiguration
-
     val rawFir = session.buildFirFromKtFiles(allSourceFiles)
 
     val (scopeSession, fir) = session.runResolution(rawFir)
